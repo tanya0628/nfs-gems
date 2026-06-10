4984569111269eda4eb90dc4ccf72110a8a43777
Tanya Chauhan <tanyachauhan0628@gmail.com>
2026-06-10 23:55:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,4 +70,20 @@ prev.onclick = () => {
   slides[index].classList.add("active");
 
 };
+const videos = document.querySelectorAll('video');
 
+videos.forEach(video => {
+
+    video.addEventListener('play', () => {
+
+        videos.forEach(otherVideo => {
+
+            if(otherVideo !== video){
+                otherVideo.pause();
+            }
+
+        });
+
+    });
+
+});
